a814058ab0cf24b5bf795288833a864afe3da8c6
Taylor Bartlett <taybart@gmail.com>
2022-12-01 18:17:24 -0700

    feat: add tests for funski

--- 2022/1/src/main.rs
+++ 2022/1/src/main.rs
@@ -1,4 +1,11 @@
 fn main() -> Result<(), String> {
+    let elfs = parse_input()?;
+    println!("most cals! {}", part1(&elfs));
+    println!("top three combined! {}", part2(&elfs));
+    Ok(())
+}
+
+fn parse_input() -> Result<Vec<i32>, String> {
     let input = std::fs::read_to_string("./src/input.txt")
         .map_err(|e| format!("read file failed {}", e))?;
 
@@ -8,26 +15,36 @@ fn main() -> Result<(), String> {
         for n in elf.split("\n") {
             // sometimes splits are empty
             if n.len() > 0 {
-                match n.parse::<i32>() {
-                    Ok(num) => {
-                        elfs[i] += num;
-                    }
-                    Err(e) => {
-                        println!("{}", e)
-                    }
-                }
+                elfs[i] += n
+                    .parse::<i32>()
+                    .map_err(|e| format!("unable to parse string {}", e))?;
             }
         }
     }
 
     elfs.sort();
+    Ok(elfs)
+}
 
-    // part 1
-    println!("most cals! {}", elfs.last().unwrap());
-    // part2
-    println!(
-        "top three combined! {}",
-        elfs[elfs.len() - 3..elfs.len()].iter().sum::<i32>()
-    );
-    Ok(())
+fn part1(elfs: &Vec<i32>) -> i32 {
+    match elfs.last() {
+        Some(i) => *i,
+        None => -1,
+    }
+}
+fn part2(elfs: &Vec<i32>) -> i32 {
+    elfs[elfs.len() - 3..elfs.len()].iter().sum::<i32>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_correctness() -> Result<(), String> {
+        let elfs = parse_input()?;
+        assert_eq!(part1(&elfs), 69281);
+        assert_eq!(part2(&elfs), 201524);
+        Ok(())
+    }
 }
